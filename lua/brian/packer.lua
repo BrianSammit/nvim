6dce0bfdb67a51f31c0be870fe723f28368f810d
BrianSammit <briansammit.cruz@gmail.com>
2022-12-17 12:42:26 -0500

    Add harpoon

--- lua/brian/packer.lua
+++ lua/brian/packer.lua
@@ -77,6 +77,7 @@ return packer.startup(function(use)
       'nvim-treesitter/nvim-treesitter',
       run = ':TSUpdate'
   }
+  use'theprimeagen/harpoon'
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
