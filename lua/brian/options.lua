26741af19c4fc1c0f54ea8309fbf05fb7031feae
brian <briansammit.cruz@gmail.com>
2025-03-03 16:04:44 +0000

    + Add clipboard

--- lua/brian/options.lua
+++ lua/brian/options.lua
@@ -32,3 +32,17 @@ vim.g.mapleader = " "
 
 vim.opt.splitbelow = true                       -- force all horizontal splits to go below current window
 vim.opt.splitright = true
+
+
+vim.g.clipboard = {
+  name = 'WslClipboard',
+  copy = {
+    ['+'] = 'clip.exe',
+    ['*'] = 'clip.exe',
+  },
+  paste = {
+    ['+'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+    ['*'] = 'powershell.exe -NoLogo -NoProfile -c [Console]::Out.Write($(Get-Clipboard -Raw).tostring().replace("`r", ""))',
+  },
+  cache_enabled = 0,
+}
